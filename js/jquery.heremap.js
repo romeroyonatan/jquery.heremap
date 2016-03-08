0de11c633e92512301a9dd65b757f54719fbf575
Yonatan Romero <yromero@openmailbox.org>
2016-03-08 14:49:31 -0300

    Muestra 3 mapas distintos en la misma ventana

--- js/jquery.heremap.js
+++ js/jquery.heremap.js
@@ -4,17 +4,7 @@
  * Licensed under the GPLv3 license.
  */
 (function($) {
-  var init, maptypes, options, platform;
-  $.fn.heremap.options = {
-    app_id: '',
-    app_code: '',
-    punctuation: '.',
-    zoom: 10,
-    center: {
-      lng: 0,
-      lat: 0
-    }
-  };
+  var getCenter, init, maptypes, options, platform;
   options = {};
   platform = null;
   maptypes = null;
@@ -22,7 +12,7 @@
   /*
    * Initialize plugin
    */
-  init = function() {
+  init = function(options) {
     if (typeof H === "undefined" || H === null) {
       throw new Error("Don't forget include 'http://js.api.here.com/v3/3.0/mapsjs-core.js'");
     }
@@ -32,16 +22,43 @@
     });
     return maptypes = platform.createDefaultLayers();
   };
-  return $.fn.heremap = function(options) {
+  getCenter = function(elem, options) {
+    var match, pattern, str;
+    str = $(elem).attr('data-center');
+    if (str != null) {
+      pattern = /(-?\d+.?\d*),(-?\d+.?\d*)/;
+      match = pattern.exec(str);
+      return {
+        lat: parseFloat(match[1]),
+        lng: parseFloat(match[2])
+      };
+    }
+    return options.center;
+  };
+  $.fn.heremap = function(options) {
     options = $.extend({}, $.fn.heremap.options, options);
-    init();
+    init(options);
     return this.each(function() {
-      var elem, map;
-      elem = $(this);
-      return map = new H.Map(elem, maptypes.normal.map, {
+      var cfg;
+      cfg = {
         zoom: options.zoom,
-        center: options.center
-      });
+        center: getCenter(this, options)
+      };
+      console.log(cfg.center);
+      return new H.Map(this, maptypes.normal.map, cfg);
     });
   };
+  return $.fn.heremap.options = {
+    app_id: '',
+    app_code: '',
+    zoom: 10,
+    center: {
+      lat: -34.6059,
+      lng: -58.3778
+    }
+  };
 })(jQuery);
+
+jQuery(document).on('ready', function() {
+  return $("div[role='map']").heremap();
+});
--- src/jquery.heremap.coffee
+++ src/jquery.heremap.coffee
@@ -5,23 +5,13 @@
 
 (($) ->
 
-  # Default options.
-  $.fn.heremap.options =
-    app_id: ''
-    app_code: ''
-    punctuation: '.'
-    zoom: 10
-    center:
-      lng: 0
-      lat: 0
   options = {}
   platform = null
   maptypes = null
-
   ###
   # Initialize plugin
   ###
-  init = ->
+  init = (options)->
     # check if H exists in the context
     if not H?
       throw new Error "Don't forget include
@@ -33,17 +23,41 @@
 
     # load map types
     maptypes = platform.createDefaultLayers()
+  
+  getCenter = (elem, options) ->
+    str = $(elem).attr('data-center')
+    if str?
+      pattern = /(-?\d+.?\d*),(-?\d+.?\d*)/
+      match = pattern.exec str
+      return {
+        lat: parseFloat match[1]
+        lng: parseFloat match[2]
+      }
+    options.center
 
 
   $.fn.heremap = (options) ->
     # Override default options with passed-in options.
     options = $.extend({}, $.fn.heremap.options, options)
-    init()
-    
+    init options
     @.each ->
-      elem = $ this
-      map = new H.Map elem, maptypes.normal.map,
+      cfg =
         zoom: options.zoom
-        center: options.center
+        center: getCenter @, options
+      console.log cfg.center
+      new H.Map @, maptypes.normal.map, cfg
+
+
+  # Default options.
+  $.fn.heremap.options =
+    app_id: ''
+    app_code: ''
+    zoom: 10
+    center:
+      lat: -34.6059
+      lng: -58.3778
 
 ) jQuery
+
+jQuery(document).on 'ready', ->
+  $("div[role='map']").heremap()
